2cd3b94de6728ace984827d431b2181c0e409867
chris <christianpimpo304@hotmail.com>
2025-06-03 16:28:53 -0300

    validators

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,15 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Req } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable({})
 export class AuthService{
+    constructor(private prisma: PrismaService) {}
 
-    login(){
-
+    signin(){
+        return {msg: 'signed in'}
     }
 
     signup(){
-        
+        return {msg: 'signed up'}
     }
 }
